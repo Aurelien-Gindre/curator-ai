4345033027540bf203c4f660f1889e354d85d4bf
louis.ferry@telecomnancy.eu <louis.ferry@telecomnancy.eu>
2024-10-31 16:16:21 +0100

    fix : wrong name in library import

--- src/conversational_agent/test.ts
+++ src/conversational_agent/test.ts
@@ -1,14 +1,12 @@
-
-
-import { createSubscriber, readSubscribers, updateSubscriber, deleteSubscriber } from './bd_tools'; // Assurez-vous que le chemin est correct
+import * as BD_TOOLS from './bd_tools'; // Assurez-vous que le chemin est correct
 
 async function main() {
     // Supprimer un abonné (remplacez par un ID valide)
-    const deletedSubscriber = await deleteSubscriber(38);
+    const deletedSubscriber = await BD_TOOLS.deleteSubscriber(34);
     console.log('Supprimer un abonné:', deletedSubscriber);
 
     // Lire les abonnés
-    const subscribers = await readSubscribers();
+    const subscribers = await BD_TOOLS.readSubscribers();
     console.log('Lire les abonnés:', subscribers);
 }
 
